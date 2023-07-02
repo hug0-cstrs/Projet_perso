16c811d915fa12f3691a2037ed2b2cd909bed56f
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-07-02 17:48:02 +0200

    Update app.js

--- Snake Game/app.js
+++ Snake Game/app.js
@@ -1,92 +1,109 @@
-const canvas = document.querySelector("canvas")
-const context = canvas.getContext('2d')
+	const canvas = document.querySelector("canvas")
+	const context = canvas.getContext('2d')
 
-let box = 20
+	let box = 20
 
-let snake = [];
-sanke[0] = { x: 10 * box, y: 10 * box }
+	let snake = [];
+	snake[0] = { x: 10 * box, y: 10 * box }
 
-let food = {
-	x: Math.floor(Math.random() * 15 + 1) * box,
-	y: Math.floor(Math.random() * 15 + 1) * box
-}
-
-let score = 0
-
-let d
-
-document.addEventListener("keydown", direction);
-
-function direction(event) {
-	let key = event.keyCode;
-	if (key == 37 && d != "RIGHT") {
-		d = "LEFT";
-	} else if (key == 38 && d != "DOWN") {
-		d = "UP";
-	} else if (key == 39 && d != "LEFT") {
-		d = "RIGHT";
-	} else if (key == 40 && d != "UP") {
-		d = "Down";
-	}
-}
-
-function draw() {
-	context.clearRect(0, 0, 400, 400)
-
-	for (let i = 0; i < snake.length; i++) {
-		context.fillStyle = (i == 0) ? "green" : "white"
-		context.fillRect(snake[i].x, snake[i].y, box, box)
-		context.strokeStyle = "red"
-		context.strokeRect(snake[i].x, snake[i].y, box, box);
+	let food = {
+		x: Math.floor(Math.random() * 15 + 1) * box,
+		y: Math.floor(Math.random() * 15 + 1) * box
 	}
 
-	context.fillStyle = "orange"
-	context.fillRect(food.x, food.y, x, y);
+	let score = 0
 
-	let snakeX = snake[0].x
-	let snakeY = snake[0].y
+	let d
 
-	if (d == "LEFT") snakeX -= box;
-	if (d == "UP") snakeY -= box;
-	if (d == "RIGHT") snakeX += box;
-	if (d == "DOWN") snakeY += box;
+	document.addEventListener("keydown", function(event){
+		if (event.keyCode === 13) { // Vérifie si la touche appuyée est "Entrée"
+        		clearInterval(game); // Arrête le jeu actuel
+        		startGame(); // Redémarre le jeu
+    		} else {
+        		direction(event); // Gère la direction du serpent pour les autres touches
+    		}
+	});
 
-	if (snakeX == food.x && snakeY == food.y) {
-		score++;
+	function direction(event) {
+		let key = event.keyCode;
+		if (key == 37 && d != "RIGHT") {
+			d = "LEFT";
+		} else if (key == 38 && d != "DOWN") {
+			d = "UP";
+		} else if (key == 39 && d != "LEFT") {
+			d = "RIGHT";
+		} else if (key == 40 && d != "UP") {
+			d = "DOWN";
+		}
+	}
+
+	function startGame() {
+		snake = [];
+		snake[0] = { x: 10 * box, y: 10 * box };
 		food = {
-			x: Math.floor(Math.random() * 15 + 1) * box,
-			y: Math.floor(Math.random() * 15 + 1) * box,
+		    x: Math.floor(Math.random() * 15 + 1) * box,
+		    y: Math.floor(Math.random() * 15 + 1) * box
+		};
+		score = 0;
+		d = undefined;
+		game = setInterval(draw, 100);
+	    }
+
+	function draw() {
+		context.clearRect(0, 0, 400, 400)
+
+		for (let i = 0; i < snake.length; i++) {
+			context.fillStyle = (i == 0) ? "green" : "white"
+			context.fillRect(snake[i].x, snake[i].y, box, box)
+			context.strokeStyle = "red"
+			context.strokeRect(snake[i].x, snake[i].y, box, box);
 		}
-	} else {
-		snake.pop()
-	}
 
-	let newHead = {
-		x: snakeX,
-		y: snakeY,
-	}
+		context.fillStyle = "orange"
+		context.fillRect(food.x, food.y, box, box);
 
-	if (snakeX < 0 || snakeY < 0 || snakeX > 19 * box || snakeY > 19 * box || collision(newHead, snake)) {
-		clearInterval(game);
-		alert("Perdu !")
-	}
+		let snakeX = snake[0].x
+		let snakeY = snake[0].y
 
-	snake.unshift(newHead);
+		if (d == "LEFT") snakeX -= box;
+		if (d == "UP") snakeY -= box;
+		if (d == "RIGHT") snakeX += box;
+		if (d == "DOWN") snakeY += box;
 
-	context.fillStyle = "red"
-	context.font = "30px Arial"
-	context.fillText(score, 2 * box, 1.6 * box)
-}
-
-function collision(head, array) {
-	for (let g = 0; g < array.length; g++) {
-		if (head.x == array[g].x && head.y == array[g].y) {
-			return true;
+		if (snakeX == food.x && snakeY == food.y) {
+			score++;
+			food = {
+				x: Math.floor(Math.random() * 15 + 1) * box,
+				y: Math.floor(Math.random() * 15 + 1) * box,
+			}
+		} else {
+			snake.pop()
 		}
+
+		let newHead = {
+			x: snakeX,
+			y: snakeY,
+		}
+
+		if (snakeX < 0 || snakeY < 0 || snakeX >= 20 * box || snakeY >= 20 * box || collision(newHead, snake)) {
+			clearInterval(game);
+			alert("Game Over !");
+		}
+
+		snake.unshift(newHead);
+
+		context.fillStyle = "red"
+		context.font = "30px Arial"
+		context.fillText(score, 2 * box, 1.6 * box)
 	}
-	return false
-}
 
-let game = setInterval(draw, 100)
-
+	function collision(head, array) {
+		for (let g = 0; g < array.length; g++) {
+			if (head.x === array[g].x && head.y === array[g].y) {
+				return true;
+			}
+		}
+		return false
+	}
+		let game = setInterval(draw, 100)
 

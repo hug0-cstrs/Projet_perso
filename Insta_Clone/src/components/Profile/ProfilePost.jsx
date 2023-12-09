c78f6da399c1ea1dc26ed921b3ef11d22ca6fbaa
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-09 19:41:53 +0100

    update file

--- Insta_Clone/src/components/Profile/ProfilePost.jsx
+++ Insta_Clone/src/components/Profile/ProfilePost.jsx
@@ -1,8 +1,78 @@
-    
-const ProfilePost = () => {
-  return (
-    <div>ProfilePost</div>
-  )
-}
+import { Image, Flex, GridItem, Text, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody} from "@chakra-ui/react";
+import { AiFillHeart } from "react-icons/ai";
+import { FaComment } from "react-icons/fa";
 
-export default ProfilePost
+const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            {/* Like Icon */}
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+
+            {/* Comment Icon */}
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        {/* Image */}
+        <Image
+          src={img}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal isOpen={isOpen} onClose={onClose} >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+
+          </ModalBody>
+
+          
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfilePost;

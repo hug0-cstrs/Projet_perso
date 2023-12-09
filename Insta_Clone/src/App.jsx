76bc997f0c26e75d45be426d84022a6ad74dbdd1
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-09 13:59:04 +0100

    insta-clone

---
+++ Insta_Clone/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
+import AuthPage from "./pages/AuthPage/AuthPage"
+import PageLayout from "./Layouts/PageLayout/PageLayout";
+
+
+function App() {
+  return (
+    <PageLayout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
+      </Routes>
+    </PageLayout>
+  )
+}
+
+export default App;
---
+++ Insta_Clone/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,30 @@
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react"
+import AuthForm from "../../components/AuthForm/AuthForm"
+
+const AuthPage = () => {
+  return (
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+        <Container maxW={"container.md"} padding={0}>
+            <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                {/* Left hand-side */}
+                <Box display={{base:"none",md:"block"}}>
+                    <Image src="./public/auth.png" h={650} alt='Phone image'/>
+                </Box>
+
+                {/* Right hand-side */}
+                <VStack spacing={4} align={"stretch"}>
+                    <AuthForm />
+                    <Box textAlign={"center"}>Get the app.</Box>
+                    <Flex gap={5} justifyContent={"center"}>
+                        <Image src="/public/playstore.png" h={"10"} alt="Playstore logo" />
+                        <Image src="/public/microsoft.png" h={"10"} alt="Microsoft logo" />
+                    </Flex>
+                </VStack>
+            </Flex>
+            
+        </Container>
+    </Flex>
+  )
+}
+
+export default AuthPage

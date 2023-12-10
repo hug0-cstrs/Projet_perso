4f4e777ce635efe591d29b38bd95c0026c42ee82
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-11 00:19:42 +0100

    Refactor PageLayout component to improve code organization and readability

--- Insta_Clone/src/Layouts/PageLayout/PageLayout.jsx
+++ Insta_Clone/src/Layouts/PageLayout/PageLayout.jsx
@@ -1,39 +1,44 @@
-import { Flex, Box, Spinner } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import Navbar from "../../components/Navbar/Navbar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/Navbar/Navbar";
+
+// instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
   const [user, loading] = useAuthState(auth);
   const canRenderSidebar = pathname !== "/auth" && user;
   const canRenderNavbar = !user && !loading && pathname !== "/auth";
-  const checkIfUserIsAuth = !user && loading;
 
-  if (checkIfUserIsAuth) return <PageLayoutSpinner />;
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
 
-  <Flex flexDir={canRenderNavbar ? "column" : "row"}>
-    {/* Left sidebar */}
-    {canRenderSidebar ? (
-      <Box w={{ base: "70px", md: "240px" }}>
-        <Sidebar />
+  return (
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
+
+      {/* sidebar on the left */}
+      {canRenderSidebar ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <Sidebar />
+        </Box>
+      ) : null}
+
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
+      
+      {/* the page content on the right */}
+      <Box
+        flex={1}
+        w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}
+        mx={"auto"}
+      >
+        {children}
       </Box>
-    ) : null}
-
-    {/* Navbar */}
-    {canRenderNavbar ? <Navbar /> : null}
-
-    {/* Page content on Right hand-side */}
-    <Box
-      flex={1}
-      w={{ base: "calc(100% - 70px", md: "calc(100% - 240px" }}
-      mx={"auto"}
-    >
-      {children}
-    </Box>
-  </Flex>;
+    </Flex>
+  );
 };
 
 export default PageLayout;
@@ -41,10 +46,10 @@ export default PageLayout;
 const PageLayoutSpinner = () => {
   return (
     <Flex
-      flexDir={"column"}
-      h={"100vh"}
-      justifyContent={"center"}
-      alignItems={"center"}
+      flexDir="column"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
     >
       <Spinner size="xl" />
     </Flex>

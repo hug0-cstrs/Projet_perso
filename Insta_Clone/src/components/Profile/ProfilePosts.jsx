95b2aca1d53fa2b8d799844d819c186e39f09a0a
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-10 23:25:34 +0100

    Refactor ProfilePosts component

--- Insta_Clone/src/components/Profile/ProfilePosts.jsx
+++ Insta_Clone/src/components/Profile/ProfilePosts.jsx
@@ -1,19 +1,18 @@
-import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
-import ProfilePost from "./ProfilePost"
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ProfilePost from "./ProfilePost";
 
 const ProfilePosts = () => {
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    },2000)
-  },[])
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   return (
-    <Grid 
+    <Grid
       templateColumns={{
         sm: "repeat(1, 1fr)",
         md: "repeat(3, 1fr)",
@@ -21,24 +20,29 @@ const ProfilePosts = () => {
       gap={1}
       columnGap={1}
     >
-      {isLoading && [0,1,2,3,4,5].map((_,idx) => (
-        <VStack key={idx} alignItems={"flex-start"} gap={4}>
-          <Skeleton w={"full"}>
-            <Box h="300px">contents wrapped</Box>
-          </Skeleton>
-        </VStack>
-      ))}
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems={"flex-start"} gap={4}>
+            <Skeleton w={"full"}>
+              <Box h="300px">contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
 
       {!isLoading && (
         <>
-        <ProfilePost img="/img1.png" />
-        <ProfilePost img="/img2.png" />
-        <ProfilePost img="/img3.png" />
-        <ProfilePost img="/img4.png" />
+          <ProfilePost img="/img1.png" />
+          <ProfilePost img="/post1.png" />
+          <ProfilePost img="/post2.png" />
+          <ProfilePost img="/post3.png" />
+          <ProfilePost img="/post4.png" />
+          <ProfilePost img="/post5.png" />
+          <ProfilePost img="/post6.png" />
+          <ProfilePost img="/post7.png" />
         </>
       )}
     </Grid>
-  )
-}
+  );
+};
 
-export default ProfilePosts
+export default ProfilePosts;

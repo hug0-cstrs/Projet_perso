292594e9028447bcc7f35ee7123c5768726544a3
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-11 00:11:22 +0100

    Update routing logic in App component

--- Insta_Clone/src/App.jsx
+++ Insta_Clone/src/App.jsx
@@ -1,15 +1,19 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
 import AuthPage from "./pages/AuthPage/AuthPage"
 import PageLayout from "./Layouts/PageLayout/PageLayout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import useAuthStore from "./store/authStore";
 
 function App() {
+
+  const authUser = useAuthStore(state => state.user)
+
   return (
     <PageLayout>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/auth" />} />
+        <Route path='/auth' element={!authUser ? <AuthPage /> : <Navigate to="/" />} />
         <Route path='/:username' element={<ProfilePage />} />
       </Routes>
     </PageLayout>

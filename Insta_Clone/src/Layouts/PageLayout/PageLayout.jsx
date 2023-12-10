638999680536cd0d9ce3b46440662d561f057a40
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-11 00:11:47 +0100

    Refactor PageLayout component to include Navbar and loading spinner

--- Insta_Clone/src/Layouts/PageLayout/PageLayout.jsx
+++ Insta_Clone/src/Layouts/PageLayout/PageLayout.jsx
@@ -1,24 +1,52 @@
-import { Flex, Box } from "@chakra-ui/react"
-import Sidebar from "../../components/Sidebar/Sidebar"
-import { useLocation } from "react-router-dom"
+import { Flex, Box, Spinner } from "@chakra-ui/react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase";
 
-const PageLayout = ({children}) => {
-    const {pathname} = useLocation();
-    return (
-        <Flex>
+const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
+  const [user, loading] = useAuthState(auth);
+  const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+  const checkIfUserIsAuth = !user && loading;
 
-            {/* Left sidebar */}
-            {pathname !== '/auth' ? (
-                <Box w={{base:"70px",md:"240px"}}>
-                    <Sidebar />
-                </Box>
-            ) : null}
-            {/* Page content on Right hand-side */}
-            <Box flex={1} w={{base:"calc(100% - 70px",md:"calc(100% - 240px"}}>
-                {children}
-            </Box>
-        </Flex>
-    )
-}
+  if (checkIfUserIsAuth) return <PageLayoutSpinner />;
 
-export default PageLayout
+  <Flex flexDir={canRenderNavbar ? "column" : "row"}>
+    {/* Left sidebar */}
+    {canRenderSidebar ? (
+      <Box w={{ base: "70px", md: "240px" }}>
+        <Sidebar />
+      </Box>
+    ) : null}
+
+    {/* Navbar */}
+    {canRenderNavbar ? <Navbar /> : null}
+
+    {/* Page content on Right hand-side */}
+    <Box
+      flex={1}
+      w={{ base: "calc(100% - 70px", md: "calc(100% - 240px" }}
+      mx={"auto"}
+    >
+      {children}
+    </Box>
+  </Flex>;
+};
+
+export default PageLayout;
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir={"column"}
+      h={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Spinner size="xl" />
+    </Flex>
+  );
+};

dd66d344f1460251435acc6cc251e01aa99c5ac6
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-12 23:03:52 +0100

    Add error handling and loading state to Login component

--- Insta_Clone/src/components/AuthForm/Login.jsx
+++ Insta_Clone/src/components/AuthForm/Login.jsx
@@ -1,12 +1,15 @@
-import { Input, Button } from "@chakra-ui/react";
+import { Input, Button, Alert, AlertIcon } from "@chakra-ui/react";
 
 import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
   });
+
+  const { loading, error, login } = useLogin();
 
   return (
     <>
@@ -27,7 +30,21 @@ const Login = () => {
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
 
-      <Button w="full" colorScheme="blue" size={"sm"} fontSize={14}>
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+
+      <Button
+        w="full"
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+        isLoading={loading}
+        onClick={() => login(inputs)}
+      >
         Log in
       </Button>
     </>

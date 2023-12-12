f26a67ad467b3e6083e7ea7558971e6272e33ee2
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-12 23:03:45 +0100

    Update SuggestedHeader component with logout functionality

--- Insta_Clone/src/components/SuggestedUsers/SuggestedHeader.jsx
+++ Insta_Clone/src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,28 +1,40 @@
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react"
-import { Link as RouterLink } from "react-router-dom"
+import { Avatar, Button, Flex, Text } from "@chakra-ui/react";
+import useLogout from "../../hooks/useLogout";
+import useAuthStore from "../../store/authStore";
+import { Link } from "react-router-dom";
 
 const SuggestedHeader = () => {
+  const { handleLogout, isLoggingOut } = useLogout();
+
+  const authUser = useAuthStore((state) => state.user);
+
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
-        <Flex alignItems={"center"} gap={2}>
-            <Avatar name="As a programmer" size={"lg"} src="/profilepic.png" />
-            <Text fontSize={12} fontWeight={"bold"}>
-                Hugo Castéras
-            </Text>
-        </Flex>
-        <Link 
-        as={RouterLink}
-        to={"/auth"}
+      <Flex alignItems={"center"} gap={2}>
+        <Link to={`${authUser.username}`}>
+          <Avatar size={"lg"} src={authUser.profilePicURL} />
+        </Link>
+        <Link to={`${authUser.username}`}>
+          <Text fontSize={12} fontWeight={"bold"}>
+            {authUser.username}
+          </Text>
+        </Link>
+      </Flex>
+      <Button
+        size={"sm"}
+        background={"transparent"}
+        _hover={{ background: "transparent" }}
         fontSize={14}
         fontWeight={"medium"}
         color={"blue.400"}
-        style={{textDecoration:"none"}}
         cursor={"pointer"}
-        >
-            Log out
-        </Link>
+        onClick={handleLogout}
+        isLoading={isLoggingOut}
+      >
+        Log out
+      </Button>
     </Flex>
-  )
-}
+  );
+};
 
-export default SuggestedHeader
+export default SuggestedHeader;

834451444ca65dcfe267434ca5afb6a662dfcae4
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-12 23:04:08 +0100

    Add firebase authentication and useAuthState hook

--- Insta_Clone/src/App.jsx
+++ Insta_Clone/src/App.jsx
@@ -3,11 +3,12 @@ import HomePage from "./pages/HomePage/HomePage"
 import AuthPage from "./pages/AuthPage/AuthPage"
 import PageLayout from "./Layouts/PageLayout/PageLayout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import useAuthStore from "./store/authStore";
+import { auth } from "./firebase/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
 
-  const authUser = useAuthStore(state => state.user)
+  const [authUser] = useAuthState(auth);
 
   return (
     <PageLayout>

e7669110427eda82e0113b1b8f0727e4b08c95d3
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-16 15:08:19 +0100

    Refactor ProfileHeader component and add user-specific functionality

--- Insta_Clone/src/components/Profile/ProfileHeader.jsx
+++ Insta_Clone/src/components/Profile/ProfileHeader.jsx
@@ -1,54 +1,109 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import {
+  Avatar,
+  AvatarGroup,
+  Button,
+  Flex,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from "../../store/authStore";
 
 const ProfileHeader = () => {
-    return (
-        <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column",sm:"row"}}>
+  const { userProfile } = useUserProfileStore();
+  const authUser = useAuthStore((state) => state.user);
+  const visitingAnotherProfileAndAuth =
+    authUser && authUser.username !== userProfile.username;
 
-            <AvatarGroup size={{base:"xl",md:"2xl"}} justifyContent={"center"} alignSelf={"flex-start"} mx={"auto"}>
-                <Avatar name="Hugo Castéras" src="/profilepic.png" alt="Hugo Castéras logo" />
-            </AvatarGroup>
+  const visitingOwnProfileAndAuth =
+    authUser && authUser.username === userProfile.username;
 
-            <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
-                <Flex gap={4} direction={{base:"column",sm:"row"}} justifyContent={{base:"center",sm:"flex-start"}} alignItems={"center"} w={"full"}>
-                    
-                    <Text fontSize={{base:"sm",md:"lg"}}>
-                    Hugo Castéras
-                    </Text>
+  return (
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+    >
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        justifyContent={"center"}
+        alignSelf={"flex-start"}
+        mx={"auto"}
+      >
+        <Avatar src={userProfile.profileOicURL} alt="Hugo Castéras logo" />
+      </AvatarGroup>
 
-                    <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-                        <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
-                            Edit Profile
-                        </Button>
-                    </Flex>
-                </Flex>
-
-                <Flex alignItems={"center"} gap={{base:2,sm:4}}>
-                    {/* posts */}
-                    <Text fontSize={{base:"xs",md:"sm"}}>
-                        <Text as="span" fontWeight={"bold"} mr={1}>4</Text>
-                        Posts
-                    </Text>
-
-                    {/* followers */}
-                    <Text fontSize={{base:"xs",md:"sm"}}>
-                        <Text as="span" fontWeight={"bold"} mr={1}>249</Text>
-                        Followers
-                    </Text>
-
-                    {/* following */}
-                    <Text fontSize={{base:"xs",md:"sm"}}>
-                        <Text as="span" fontWeight={"bold"} mr={1}>179</Text>
-                        Following
-                    </Text>
-                </Flex>
-
-                <Flex alignItems={"center"} gap={4}>
-                    <Text fontSize={"sm"} fontWeight={"bold"}>Student Programmer</Text>
-                </Flex>
-                <Text fontSize={"sm"} >Tutorials to improve my skills as programmer</Text>
-            </VStack>
+      <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+        <Flex
+          gap={4}
+          direction={{ base: "column", sm: "row" }}
+          justifyContent={{ base: "center", sm: "flex-start" }}
+          alignItems={"center"}
+          w={"full"}
+        >
+          <Text fontSize={{ base: "sm", md: "lg" }}>{userProfile.username}</Text>
+            {visitingOwnProfileAndAuth && (
+              <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                <Button
+                  bg={"white"}
+                  color={"black"}
+                  _hover={{ bg: "whiteAlpha.800" }}
+                  size={{ base: "xs", md: "sm" }}
+                >
+                  Edit Profile
+                </Button>
+              </Flex>
+            )}
+            {visitingAnotherProfileAndAuth && (
+              <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                <Button
+                  bg={"blue.500"}
+                  color={"white"}
+                  _hover={{ bg: "blue.600" }}
+                  size={{ base: "xs", md: "sm" }}
+                >
+                  Follow
+                </Button>
+              </Flex>
+            )}
         </Flex>
-    )
-}
 
-export default ProfileHeader
+        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
+          {/* posts */}
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {userProfile.posts.length}
+            </Text>
+            Posts
+          </Text>
+
+          {/* followers */}
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {userProfile.followers.length}
+            </Text>
+            Followers
+          </Text>
+
+          {/* following */}
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {userProfile.following.length}
+            </Text>
+            Following
+          </Text>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={4}>
+					<Text fontSize={"sm"} fontWeight={"bold"}>
+						{userProfile.fullname}
+					</Text>
+				</Flex>
+				<Text fontSize={"sm"}>{userProfile.bio}</Text>
+			</VStack>
+    </Flex>
+  );
+};
+
+export default ProfileHeader;

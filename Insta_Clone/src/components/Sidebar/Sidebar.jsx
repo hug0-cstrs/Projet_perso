cd397c7f2b3895bf71128d4704ba2b232c4538ac
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-17 19:18:39 +0100

    Refactor Sidebar component

--- Insta_Clone/src/components/Sidebar/Sidebar.jsx
+++ Insta_Clone/src/components/Sidebar/Sidebar.jsx
@@ -1,43 +1,16 @@
-import { Avatar, Box, Flex, Link, Tooltip, Button } from "@chakra-ui/react";
+import { Box, Flex, Link, Tooltip, Button } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import {
-  CreatePostLogo,
   InstagramLogo,
   InstagramMobileLogo,
-  NotificationsLogo,
-  SearchLogo,
 } from "../../assets/constants";
 
-import { AiFillHome } from "react-icons/ai";
 import { BiLogOut } from "react-icons/bi";
 
 import useLogout from "../../hooks/useLogout";
+import SiderbarItems from "./SiderbarItems";
 
 const Sidebar = () => {
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25} />,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo />,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo />,
-      text: "Notifications",
-    },
-    {
-      icon: <CreatePostLogo />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="Burak Ormez" src="/profilepic.png" />,
-      text: "Profile",
-      link: "/asaprogrammer",
-    },
-  ];
 
   const { handleLogout, isLoggingOut } = useLogout();
 
@@ -75,33 +48,7 @@ const Sidebar = () => {
           <InstagramMobileLogo />
         </Link>
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={500}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || "/"}
-                as={RouterLink}
-                alignItems={"center"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: 10, md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SiderbarItems />
         </Flex>
 
         {/* LOGOUT */}
